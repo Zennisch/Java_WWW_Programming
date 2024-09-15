88701cdacb70ee6a61619134f19afc9be52ea7d8
Cosmic Reina <snowy.sunny.dazing.us@gmail.com>
2024-09-15 14:54:32 +0900

    Finish W01_E04

--- W01_E04/src/main/java/servlet/Register.java
+++ W01_E04/src/main/java/servlet/Register.java
@@ -1,28 +1,40 @@
 package servlet;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 @WebServlet("/Register")
 public class Register extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-	public Register() {
-		super();
-	}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("Served at: " + req.getContextPath());
+    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        var writer = resp.getWriter();
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doGet(request, response);
-	}
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        String username = req.getParameter("username");
+        String email = req.getParameter("email");
+        String facebook = req.getParameter("facebook");
+        String shortBio = req.getParameter("shortBio");
 
+        var out = "<div>" + "Congratulations " + firstName + " " + lastName + ", you have successfully registered!" + "</div>"
+                + "<div>" + "Your information: "
+                + "<div>" + "Username: " + username + "</div>"
+                + "<div>" + "Email: " + email + "</div>"
+                + "<div>" + "Password: " + "<i>hidden</i>" + "</div>"
+                + "<div>" + "Facebook: " + facebook + "</div>"
+                + "<div>" + "Short Bio: " + shortBio + "</div>"
+                + "</div>";
+
+        writer.println(out);
+    }
 }
