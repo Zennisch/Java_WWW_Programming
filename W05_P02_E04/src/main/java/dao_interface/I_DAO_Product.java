2fdf53a3067388eb8134323a2d41e7977e47d615
Cosmic Reina <snowy.sunny.dazing.us@gmail.com>
2024-09-22 18:42:19 +0900

    Update I_DAO_Product.java

--- W05_P02_E04/src/main/java/dao_interface/I_DAO_Product.java
+++ W05_P02_E04/src/main/java/dao_interface/I_DAO_Product.java
@@ -5,7 +5,7 @@ import model.Product;
 import java.util.List;
 
 public interface I_DAO_Product {
-    public List<Product> getAllProducts();
-    public Product getProduct(int id);
-    public boolean addProduct(Product product);
+    List<Product> getAllProducts();
+    Product getProduct(int id);
+    boolean addProduct(Product product);
 }
