51ad49596cd718946a9a6faaa1d233060e60f918
Cosmic Reina <snowy.sunny.dazing.us@gmail.com>
2024-11-01 01:29:11 +0900

    Create Student.java

---
+++ W10_P04_E04/src/main/java/model/Student.java
@@ -0,0 +1,110 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String favoriteLanguage;
+    private String[] operatingSystems;
+
+    private HashMap<String, String> countryOptions;
+    private HashMap<String, String> favoriteLanguageOptions;
+    private List<String> operatingSystemsOptions;
+
+    public Student() {
+        countryOptions = new HashMap<>();
+        countryOptions.put("BR", "Brazil");
+        countryOptions.put("FR", "France");
+        countryOptions.put("DE", "Germany");
+        countryOptions.put("IN", "India");
+        countryOptions.put("US", "United States");
+
+        favoriteLanguageOptions = new HashMap<>();
+        favoriteLanguageOptions.put("Java", "Java");
+        favoriteLanguageOptions.put("C#", "C#");
+        favoriteLanguageOptions.put("PHP", "PHP");
+        favoriteLanguageOptions.put("Ruby", "Ruby");
+
+        operatingSystemsOptions = new ArrayList<>();
+        operatingSystemsOptions.add("Linux");
+        operatingSystemsOptions.add("Mac OS");
+        operatingSystemsOptions.add("Microsoft Windows");
+    }
+
+    public Student(String[] operatingSystems, String favoriteLanguage, String country, String lastName, String firstName) {
+        this.operatingSystems = operatingSystems;
+        this.favoriteLanguage = favoriteLanguage;
+        this.country = country;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public String[] getOperatingSystems() {
+        return operatingSystems;
+    }
+
+    public void setOperatingSystems(String[] operatingSystems) {
+        this.operatingSystems = operatingSystems;
+    }
+
+    public HashMap<String, String> getCountryOptions() {
+        return countryOptions;
+    }
+
+    public HashMap<String, String> getFavoriteLanguageOptions() {
+        return favoriteLanguageOptions;
+    }
+
+    public List<String> getOperatingSystemsOptions() {
+        return operatingSystemsOptions;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                ", favoriteLanguage='" + favoriteLanguage + '\'' +
+                ", operatingSystems=" + Arrays.toString(operatingSystems) +
+                '}';
+    }
+}
