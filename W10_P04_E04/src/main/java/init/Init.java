8c634bf58475e1bfd8a62070bf816f14bc58497a
Cosmic Reina <snowy.sunny.dazing.us@gmail.com>
2024-10-31 14:23:11 +0900

    Update

---
+++ W10_P04_E04/src/main/java/init/Init.java
@@ -0,0 +1,9 @@
+package init;
+
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+public class Init {
+//    DispatcherServlet
+//    InternalResourceViewResolver
+}
