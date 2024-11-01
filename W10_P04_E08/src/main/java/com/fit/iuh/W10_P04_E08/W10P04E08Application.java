f556829f5f9d6f3e3e75a4052155534c28a58b70
Zennisch <snowy.sunny.dazing.us@gmail.com>
2024-11-01 23:21:45 +0900

    Create W10_P04_E08

---
+++ W10_P04_E08/src/main/java/com/fit/iuh/W10_P04_E08/W10P04E08Application.java
@@ -0,0 +1,13 @@
+package com.fit.iuh.W10_P04_E08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W10P04E08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W10P04E08Application.class, args);
+	}
+
+}
---
+++ W10_P04_E08/src/test/java/com/fit/iuh/W10_P04_E08/W10P04E08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.iuh.W10_P04_E08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W10P04E08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
