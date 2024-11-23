f002d92d5bcaa9c3375e2ea370d42c94141ce890
Zennisch <snowy.sunny.dazing.us@gmail.com>
2024-11-23 13:15:13 +0900

    Create W11_P05_E01

---
+++ W11_P05_E01/src/test/java/com/fit/iuh/w11_p05_e01/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.iuh.w11_p05_e01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
