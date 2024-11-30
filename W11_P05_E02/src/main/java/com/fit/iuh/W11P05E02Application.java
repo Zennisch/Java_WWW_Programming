e5580e0dd659026d2731b43cc45621b026f41bba
Zennisch <snowy.sunny.dazing.us@gmail.com>
2024-12-01 00:44:09 +0900

    Create W11_P05_E02

---
+++ W11_P05_E02/src/main/java/com/fit/iuh/W11P05E02Application.java
@@ -0,0 +1,13 @@
+package com.fit.iuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W11P05E02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W11P05E02Application.class, args);
+	}
+
+}
---
+++ W11_P05_E02/src/test/java/com/fit/iuh/W11P05E02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.iuh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W11P05E02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
